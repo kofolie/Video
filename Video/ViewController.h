a31a1c8452eeca1801e7fe40ab02605615dd243a
Daria Konstantinova <konstantinova.daria@gmail.com>
2013-09-14 19:55:16 +0400

    first commit

---
+++ Video/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Video
+//
+//  Created by Daria Konstantinova on 9/14/13.
+//  Copyright (c) 2013 Daria Konstantinova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
